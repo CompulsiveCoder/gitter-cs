519ec344956dd7a651e3abe27350c27b318f7123
CompulsiveCoder <compulsivecoder@gmail.com>
2016-05-23 11:20:16 +1000

    Tweaked prepare script

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,6 @@ echo "Preparing for project"
 echo "Dir: $PWD"
 
 sudo apt-get update
-sudo apt-get install -y git wget mono-base
+sudo apt-get install -y git wget mono-complete
 
 mozroots --import --sync
