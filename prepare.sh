404717f12deab5abf9a6d07627d7f73b7fc7b59c
John <compulsivecoder@gmail.com>
2018-03-18 20:52:43 +0000

    Fixed prepare script

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,14 @@
 echo "Preparing for project"
 echo "Dir: $PWD"
 
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+
+# Create the source list file then copy (directly creating causes permission denied errors on c9.io)
+sudo echo "deb http://download.mono-project.com/repo/debian wheezy main" > mono-xamarin.list
+sudo cp mono-xamarin.list /etc/apt/sources.list.d/mono-xamarin.list
+
 sudo apt-get update
-sudo apt-get install -y git wget mono-complete
+sudo apt-get install -y --no-install-recommends git wget mono-complete mono-devel ca-certificates-mono
+
