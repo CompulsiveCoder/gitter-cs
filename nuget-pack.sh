a10469e48e746c9602cb0249d79de3dfac3cd81d
John <compulsivecoder@gmail.com>
2018-03-19 02:31:31 +0000

    Fixed nuget version

--- nuget-pack.sh
+++ nuget-pack.sh
@@ -5,6 +5,8 @@ mv -f pkg/*.nupkg pkg/archive
 
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
+VERSION=$(cat version.txt)
+
 if [ "$BRANCH" = "dev" ]
 then
     VERSION="$VERSION-dev"
@@ -12,7 +14,5 @@ then
     # Only increment version during dev builds. The dev version can be used when graduated to the master branch
     sh increment-version.sh
 fi
-
-VERSION=$(cat version.txt)
 
 mono lib/nuget.exe pack Package.nuspec -version $VERSION -OutputDirectory pkg/
