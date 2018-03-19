dc854cadf5cc1e90d7b8f0582d83fbbae8f04fb9
John <compulsivecoder@gmail.com>
2018-03-19 00:56:20 +0000

    Fixing nuget package and push

--- nuget-pack.sh
+++ nuget-pack.sh
@@ -3,13 +3,16 @@ mkdir -p pkg/archive
 
 mv -f pkg/*.nupkg pkg/archive
 
-VERSION=$(cat version.txt)
-
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 if [ "$BRANCH" = "dev" ]
 then
     VERSION="$VERSION-dev"
+    
+    # Only increment version during dev builds. The dev version can be used when graduated to the master branch
+    sh increment-version.sh
 fi
+
+VERSION=$(cat version.txt)
 
 mono lib/nuget.exe pack Package.nuspec -version $VERSION -OutputDirectory pkg/
--- nuget-push.sh
+++ nuget-push.sh
@@ -1,3 +1,1 @@
-sh increment-version.sh
-
 mono lib/nuget.exe push pkg/gitter*nupkg -Source https://www.nuget.org/api/v2/package
