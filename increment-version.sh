c24102f4d62163fba995eb4a4e98b519b0806b5d
John <compulsivecoder@gmail.com>
2018-03-19 06:20:42 +0000

    Cleaned up increment version script

--- increment-version.sh
+++ increment-version.sh
@@ -6,12 +6,7 @@ CURRENT_VERSION=$(cat buildnumber.txt)
 
 echo "Current: $CURRENT_VERSION"
 
-#CURRENT_VERSION=='expr $CURRENT_VERSION + 1'
 CURRENT_VERSION=$(($CURRENT_VERSION + 1))
-#echo $CURRENT_VERSION | awk 'BEGIN { FS=":" } { $3++;  if ($3 > 99) { $3=0; $2++; if ($2 > 99) { $2=0; $1++ } } } { printf "%02d:%02d:%02d\n", $1, $2, $3 }'
-#echo $CURRENT_VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(1000^length($NF))); print}' > version.txt
-
-#NEW_VERSION=$(cat version.txt)
 
 echo "New version: $CURRENT_VERSION"
 
