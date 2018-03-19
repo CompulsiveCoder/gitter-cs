6c24d24b7ab4bf22223000b4d9c83a7b4d1e2183
John <compulsivecoder@gmail.com>
2018-03-19 04:44:05 +0000

    Updated versioning

--- nuget-pack.sh
+++ nuget-pack.sh
@@ -6,13 +6,16 @@ mv -f pkg/*.nupkg pkg/archive
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 VERSION=$(cat version.txt)
+BUILD_NUMBER=$(cat buildnumber.txt)
+
+FULL_VERSION=$VERSION.$BUILD_NUMBER
 
 if [ "$BRANCH" = "dev" ]
 then
-    VERSION="$VERSION-dev"
+    FULL_VERSION="$FULL_VERSION-dev"
     
     # Only increment version during dev builds. The dev version can be used when graduated to the master branch
     sh increment-version.sh
 fi
 
-mono lib/nuget.exe pack Package.nuspec -version $VERSION -OutputDirectory pkg/
+mono lib/nuget.exe pack Package.nuspec -version $FULL_VERSION -OutputDirectory pkg/
