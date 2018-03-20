ded9983db9b7d4af5ca7e9798b86072c54c0e71e
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-20 05:34:29 +0000

    Fixed nuget pack script

--- nuget-pack.sh
+++ nuget-pack.sh
@@ -5,6 +5,12 @@ mv -f pkg/*.nupkg pkg/archive
 
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
+if [ "$BRANCH" = "dev" ]
+then    
+    # Only increment version during dev builds. The dev version can be used when graduated to the master branch
+    sh increment-version.sh
+fi
+
 VERSION=$(cat version.txt)
 BUILD_NUMBER=$(cat buildnumber.txt)
 
@@ -13,9 +19,6 @@ FULL_VERSION=$VERSION.$BUILD_NUMBER
 if [ "$BRANCH" = "dev" ]
 then
     FULL_VERSION="$FULL_VERSION-dev"
-    
-    # Only increment version during dev builds. The dev version can be used when graduated to the master branch
-    sh increment-version.sh
 fi
 
 mono lib/nuget.exe pack Package.nuspec -version $FULL_VERSION -OutputDirectory pkg/
