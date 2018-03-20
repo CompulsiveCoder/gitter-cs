422f1fb0f7628a05194f5df7746e702d8858f18f
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-20 04:21:26 +0000

    Updating test

--- src/gitter.Tests.Unit/BaseUnitTestFixture.cs
+++ src/gitter.Tests.Unit/BaseUnitTestFixture.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using NUnit.Framework;
+using System.IO;
 
 namespace gitter.Tests.Unit
 {
     public abstract class BaseUnitTestFixture : BaseTestFixture
     {
+		public string OriginalDirectory { get; set; }
+
         public BaseUnitTestFixture ()
         {
         }
+
+		[SetUp]
+		public void Setup()
+		{
+			OriginalDirectory = Environment.CurrentDirectory;
+			
+			Environment.CurrentDirectory = new TemporaryDirectoryCreator ().Create ();
+
+			Console.WriteLine ("Current directory:");
+			Console.WriteLine (Environment.CurrentDirectory);
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			var tmpDir = Environment.CurrentDirectory;
+
+			Environment.CurrentDirectory = OriginalDirectory;
+
+			Directory.Delete (tmpDir, true);
+		}
     }
 }
 
